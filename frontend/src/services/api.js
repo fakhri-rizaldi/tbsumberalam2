4e4d2b478d8faa794d1220ba79e13cbcdfe9918f
Nama Anda <muhamadfakhri06@gmail.com>
2026-07-16 09:46:23 +0700

    Update API URL for Vercel deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://tbsumberalam2.test/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://tbsumberalam2.test/api',
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
